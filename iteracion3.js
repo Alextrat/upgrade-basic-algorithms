a00ace732ffd79face90b15d59a92c9f24463c10
Alextrat <alejandro.perez@bootcamp-upgrade.com>
2022-11-25 21:49:53 +0100

    Ejercicio iteración #3: Operadores

---
+++ iteracion3.js
@@ -0,0 +1,20 @@
+//1.1
+let a = 10;
+let b = 5;
+let resultado = a * b;
+console.log(resultado);
+//1.2
+console.log(a/2);
+//1.3
+var c = 15;
+var d = 9;
+var resto = 15%9;
+console.log(resto);
+//1.4
+let y = 10;
+let z = 5;
+let x = y+z;
+console.log(x)
+//1.5 
+x= y*z
+console.log(x)
