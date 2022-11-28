bd0215b3b872656f1dffae37c7b56fe0dcf8b57f
Alextrat <alejandro.perez@bootcamp-upgrade.com>
2022-11-28 21:17:52 +0100

    Añado ejercicio iteracion2

---
+++ prueba.js
@@ -0,0 +1,11 @@
+let direccion2 = {
+    calle : "Orense",
+    numero : 69,
+    codigoPostal : "28000",
+    ciudad : "Madrid" ,
+}
+
+console.log(direccion2.ciudad);
+
+direccion2.pais = "España";
+console.log(direccion2);
